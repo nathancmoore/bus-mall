16d75c193ac660a42ab1a82d55d3ebd1cf7d25fe
Nathan Moore <ncmooore1986@gmail.com>
2017-08-21 13:02:46 -0700

    downloaded images and built them in object literal notation in my JS file.

---
+++ app.js
@@ -0,0 +1,108 @@
+'use strict';
+
+var bag = {
+  name: 'bag',
+  path: 'img/bag.jpg',
+};
+
+var banana = {
+  name: 'banana',
+  path: 'img/banana.jpg'
+};
+
+var bathroom = {
+  name: 'bathroom',
+  path: 'img/bathroom.jpg',
+};
+
+var boots = {
+  name: 'bathroom',
+  path: 'img/bathroom.jpg',
+};
+
+var breakfast = {
+  name: 'breakfast',
+  path: 'img/breakfast.jpg',
+};
+
+var bubblegum = {
+  name: 'bubblegum',
+  path: 'img/bubblegum.jpg',
+};
+
+var chair = {
+  name: 'chair',
+  path: 'img/chair.jpg',
+};
+
+var cthulhu = {
+  name: 'cthulhu',
+  path: 'img/cthulhu.jpg',
+};
+
+var dogduck = {
+  name: 'dogduck',
+  path: 'img/dogduck.jpg',
+};
+
+var dragon = {
+  name: 'dragon',
+  path: 'img/dragon.jpg',
+};
+
+var pen = {
+  name: 'pen',
+  path: 'img/dragon.jpg',
+};
+
+var petsweep = {
+  name: 'petsweep',
+  path: 'img/petsweep.jpg',
+};
+
+var scissors = {
+  name: 'scissors',
+  path: 'img/scissors.jpg'
+};
+
+var shark = {
+  name: 'shark',
+  path: 'img/shark.jpg',
+};
+
+var sweep = {
+  name: 'sweep',
+  path: 'img/sweep.jpg',
+};
+
+var tauntaun = {
+  name: 'tauntaun',
+  path: 'img/tauntaun.jpg',
+};
+
+var unicorn = {
+  name: 'unicorn',
+  path: 'img/unicorn.jpg',
+};
+
+var usb = {
+  name: 'usb',
+  path: 'img/usb.jpg',
+};
+
+var watercan = {
+  name: 'watercan',
+  path: 'img/watercan.jpg',
+};
+
+var wineglass = {
+  name: 'wineglass',
+  path: 'img/wineglass.jpg',
+};
+// function createAppend(newChildElementTag, parentElement, newContent, newClass, newId) {
+//   var child = document.createElement(newChildElementTag);
+//   child.class = newClass;
+//   child.id = newId;
+//   child.innerText = newContent;
+//   parentElement.appendChild(child);
+// };
